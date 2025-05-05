6924bc8a45945acd8add530a9959eea56eeb8eb4
mariosabeh2022 <mariosabeh2022@gmail.com>
2025-05-05 16:49:57 +0300

    Creating the project

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  return (
+    <View style={backgroundStyle}>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
+
+export default App;
